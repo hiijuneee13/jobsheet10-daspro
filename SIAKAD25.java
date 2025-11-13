b5e309df80c7084e39006a009566801871727a5b
Rizal Maulana <xjunngoat1@gmail.com>
2025-11-13 10:11:46 +0700

    modifikasi pertanyaan percobaan 4

--- SIAKAD25.java
+++ SIAKAD25.java
@@ -1,40 +1,50 @@
 import java.util.Scanner;
 
-public class SIAKAD25 { 
+public class SIAKAD25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
-        String[] namaMatkul = {"Dasar Pemrograman", "Matematika", "CTPS"}; 
 
-       
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+        System.out.print("Masukkan jumlah siswa: ");
+        int mahasiswa = sc.nextInt();
+
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int Matkul = sc.nextInt();
+        sc.nextLine(); 
+
+        String[] namaMatkul = new String[Matkul];
+        for (int i = 0; i < Matkul; i++) {
+            System.out.print("Masukkan nama mata kuliah ke-" + (i + 1) + ": ");
+            namaMatkul[i] = sc.nextLine();
+        }
+
+        int[][] nilai = new int[mahasiswa][Matkul];
+
+        for (int i = 0; i < mahasiswa; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
             double totalPerSiswa = 0;
 
-            for (int j = 0; j < nilai[i].length; j++) {
+            for (int j = 0; j < Matkul; j++) {
                 System.out.print("Nilai mata kuliah " + namaMatkul[j] + " : ");
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
 
-            double rataPerSiswa = totalPerSiswa / nilai[i].length;
-            System.out.println("Nilai rata-rata: " + rataPerSiswa);
-            System.out.println();
+            double rataPerSiswa = totalPerSiswa / Matkul;
+            System.out.println("Rata-rata mahasiswa ke-" + (i + 1) + ": " + rataPerSiswa);
         }
 
-      
-        System.out.println("\n==============================================");
+        System.out.println("\n===========================================");
         System.out.println("Rata-rata Nilai Setiap Mata Kuliah:");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < Matkul; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < mahasiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
 
-            double rataPerMatkul = totalPerMatkul / nilai.length;
-            System.out.println("Mata Kuliah " + (j + 1) + " : " + rataPerMatkul);
+            double rataPerMatkul = totalPerMatkul / mahasiswa;
+            System.out.println(namaMatkul[j] + " : " + rataPerMatkul);
         }
 
         sc.close();
