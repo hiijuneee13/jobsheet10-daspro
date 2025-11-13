798a14e1c93cb833256c560bfbd36fdaf258a2f5
Rizal Maulana <xjunngoat1@gmail.com>
2025-11-13 10:40:08 +0700

    Modifikasi percobaan 1

--- Bioskop25.java
+++ Bioskop25.java
@@ -10,6 +10,7 @@ public class Bioskop25 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
 
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Penonton pada baris ke-" + (i+1) + " : " 
