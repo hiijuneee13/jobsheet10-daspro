a328e1135b619dcda8e7afd6d8bd44255ad0f116
Rizal Maulana <xjunngoat1@gmail.com>
2025-11-13 09:57:16 +0700

    percobaan 4

--- SIAKAD25.java
+++ SIAKAD25.java
@@ -1,27 +1,18 @@
 import java.util.Scanner;
 
-public class SIAKAD25 {
+public class SIAKAD25 { 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int siswa = 4, matkul = 3;
-        int[][] nilai = new int[siswa][matkul];
-        for (int i=0;i<siswa;i++) {
-            System.out.println("Nilai siswa "+(i+1));
-            for (int j=0;j<matkul;j++) {
-                System.out.print("Mata kuliah "+(j+1)+": ");
+        int[][] nilai = new int[4][3]; 
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + " : ");
                 nilai[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Rata-rata tiap siswa:");
-        for (int i=0;i<siswa;i++) {
-            int sum=0; for(int j=0;j<matkul;j++) sum+=nilai[i][j];
-            System.out.printf("Siswa %d : %.2f%n",i+1,(double)sum/matkul);
-        }
-        System.out.println("Rata-rata tiap mata kuliah:");
-        for (int j=0;j<matkul;j++) {
-            int sum=0; for(int i=0;i<siswa;i++) sum+=nilai[i][j];
-            System.out.printf("Matkul %d : %.2f%n",j+1,(double)sum/siswa);
-        }
+
         sc.close();
     }
 }
