7593d59560b3261a6d519060764985234fe2450e
Rizal Maulana <xjunngoat1@gmail.com>
2025-11-13 09:47:27 +0700

    percobaan 3

--- Numbers25.java
+++ Numbers25.java
@@ -1,23 +1,8 @@
-import java.util.Arrays;
-
 public class Numbers25 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
-        for (int i=0;i<myNumbers.length;i++) {
-            System.out.println("Baris "+i+" : "+Arrays.toString(myNumbers[i]));
-        }
-        myNumbers[0][2] = 99;
-        System.out.println("Setelah ubah nilai:");
-        for (int i=0;i<myNumbers.length;i++) {
-            System.out.println("Baris "+i+" : "+Arrays.toString(myNumbers[i]));
-        }
-        myNumbers[1] = new int[6];
-        System.out.println("Setelah ganti panjang baris 1:");
-        for (int i=0;i<myNumbers.length;i++) {
-            System.out.println("Baris "+i+" : "+Arrays.toString(myNumbers[i]));
-        }
     }
 }
